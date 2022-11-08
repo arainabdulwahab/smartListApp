8f9ff1c825efd253f5f56ecaf0146cfd15e620a2
Thegoldendice <devastomc@gmail.com>
2022-11-08 17:55:39 +0100

    Fix Ricette Tempo CSS

--- frontEnd/js/functions.js
+++ frontEnd/js/functions.js
@@ -98,9 +98,9 @@ function ricettes(i, recipe) {
     <div class="card bg-white shadow-lg p-0 m-1 bg-body rounded responsiveRecipe">
         <div class=" border border-0">
             <div class="p-1 mb-1  text-white position-absolute top-0 end-0 " >
-                <div class="d-flex flex-row bd-highlight mb-0 bg-dark rounded topItemBox">
+                <div class="d-flex flex-row bd-highlight mb-0 bg-dark rounded topItemBox opacity-75">
                     <div class="p-2 bd-highlight ">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
                             class="bi bi-clock " viewBox="0 0 20 15">
                             <path
                                 d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
@@ -108,7 +108,7 @@ function ricettes(i, recipe) {
                                 d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
                         </svg>
                     </div>
-                    <p class="pt-2 bd-highlight">60 min</p>
+                    <p class="pt-2 bd-highlight" style="padding-right: 1vw; margin-top: 0.5vh; font-size: small;">60 min</p>
                 </div>
             </div>
             <img src="${recipe.immagine}" class="card-img-top" alt="..."
