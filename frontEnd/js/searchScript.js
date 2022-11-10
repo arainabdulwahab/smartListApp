f5caa3832af0fed2bef1fe94b920a86bf24d7702
arainabdulwahab <113197162+arainabdulwahab@users.noreply.github.com>
2022-11-10 21:29:11 +0100

    create ui fixed

--- frontEnd/js/searchScript.js
+++ frontEnd/js/searchScript.js
@@ -43,9 +43,9 @@ let outputHTML = matches =>{
   if(matches.length > 0 ){
     let html = matches.map(match =>
       `
-    <div class="card card-body mb-1"  >
-      <img src="${match.immagine}" width="40px" height="40px">
-      <h6>${match.genere}</h6>
+    <div class=""d-flex justify-content-start">
+      <img src="${match.immagine}" width="40px" height="40px" >
+      <p class="" >${match.genere}</p>
     </div>`
     )
     .join('');
@@ -63,41 +63,39 @@ let outputHTML = matches =>{
     matchList.innerHTML = '';
   });
   addButton.addEventListener('click', function(){
-    let testDiv = document.createElement('div');
-
+    let testDiv = document.createElement('tr');
+    testDiv.id =id="itemToShow";
     let newDiv =  '';
-    newDiv +=`<div class="mt-1 mb-3 d-flex justify-content-between" id="itemToShow">
 
-      <div class=" p-2">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-          class="bi bi-x-circle" viewBox="0 0 16 16">
-          <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-          <path
-            d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-        </svg>
-      </div>
-
-      <div class=" p-2">
-        <img src="${img}" width="30px" height="30px" class="p-2 bd-highlight"
-          style="background-color: grey;">
-      </div>
-
-      <div class="p-2">
-        <p>${newValGenere}</p>
-      </div>
-
-      <div class=" bd-highlight p-2">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-      </div>
-
-      <div class="p-2">
-        <p>${quantitaInput.value}</p>
-      </div>
-
-      <div class="p-2" style="width: 10%;">
-        <p>${prezzoInput.value}</p>
-      </div>
-    </div> <!-- end righa-->`;
+    
+    newDiv+= ` 
+    <th scope="row" class="border-0 m-5">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+        class="bi bi-x-circle" viewBox="0 0 16 16">
+        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"></path>
+        <path
+          d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z">
+        </path>
+      </svg>
+    </th>
+    <td class="border-0">
+      <img src="${img}" width="35px" height="35px" class="p-2 bd-highlight"
+        style="background-color: #f2f2f2;">
+    </td>
+    <td class="border-0" style="width:50%">
+      <p class="m-6">${newValGenere} </p>
+    </td>
+    <td class="border-0">
+      <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"
+        style="background-color:rgb(19, 202, 235); border:0;" checked>
+    </td>
+    <td class="border-0">
+      <p> ${quantitaInput.value}</p>
+    </td>
+    <td class="border-0">
+      <p> ${prezzoInput.value}</p>
+    </td>
+  </tr>`;
     testDiv.innerHTML = newDiv;
   
   totalPrice.value = prezzoInput.value;
